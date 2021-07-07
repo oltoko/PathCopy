30f75a4a6c0d43f14bce6aba4bec73abe821362f
Oliver Koch <github@oltoko.de>
2021-07-07 23:23:49 +0200

    add info where issues should be placed

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ fn main() {
     let matches = App::new("PathCopy")
         .version(crate_version!()).version_short("v")
         .about("Copies the absolute Path of the given file or directory to the clipboard. It follows symlinks and uses the absolute path of the linked file.")
-        .after_help("If there are any issues, feel free to fill in a bug\nhttps://...")
+        .after_help("If there are any issues, feel free to fill in a bug\nhttps://github.com/oltoko/PathCopy/issues")
         .arg(Arg::with_name(PATH_NAME)
             .help("The file or directory from which the absolute path should be copied.")
             .required(true)
