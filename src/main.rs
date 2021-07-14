3fe0fbfc8661bb0537af2194fa765b2da4fd196b
Oliver Koch <github@oltoko.de>
2021-07-14 20:52:26 +0200

    handle several input paths

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,4 @@
 use clap::{crate_version, App, Arg};
-use clipboard::{ClipboardContext, ClipboardProvider};
-use std::path::Path;
 
 const PATH_NAME: &str = "path";
 
@@ -11,44 +9,13 @@ fn main() {
         .after_help("If there are any issues, feel free to fill in a bug\nhttps://github.com/oltoko/PathCopy/issues")
         .arg(Arg::with_name(PATH_NAME)
             .help("The file or directory from which the absolute path should be copied.")
+            .multiple(true)
             .required(true)
             .index(1))
         .get_matches();
 
-    let input_path = matches.value_of(PATH_NAME).unwrap();
-    let path = Path::new(input_path);
+    let inputs = matches.values_of(PATH_NAME).unwrap();
+    let paths = pc::to_absolut_paths(inputs);
 
-    if !path.exists() {
-        eprintln!("The given path doesn't exist!");
-        std::process::exit(1);
-    }
-
-    let path = match path.canonicalize() {
-        Ok(buf) => buf,
-        Err(e) => {
-            eprintln!("Failed to get absolute path of {}: {}", input_path, e);
-            std::process::exit(2);
-        }
-    };
-
-    let mut ctx: ClipboardContext = ClipboardProvider::new().unwrap();
-
-    let content = match path.into_os_string().into_string() {
-        Ok(s) => s,
-        Err(_) => {
-            eprintln!("Failed to place the given path into the clipboard. The absolute version might contain some unsupported characters.");
-            std::process::exit(3);
-        }
-    };
-
-    match ctx.set_contents(content) {
-        Ok(()) => (), /* Everything is fine 😌 */
-        Err(e) => {
-            eprintln!(
-                "Failed to place the absolute path of {} into the clipboard: {}",
-                input_path, e
-            );
-            std::process::exit(4);
-        }
-    };
+    pc::paste_to_clipboard(paths);
 }
